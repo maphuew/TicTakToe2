ed27e409da8ee51114349caa2a65eaf5d6db2e5c
matthew-offen <matthew.offen@xero.com>
2021-12-04 15:18:02 +1300

    Add tests and player tests

---
+++ TikTakToe2/TikTakToe2Tests/PlayerTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TikTakToe2;
+using Xunit;
+
+namespace TikTakToe2Tests
+{
+    class PlayerTests
+    {
+        public void GivenOnePlayer_AssignCharacterX()
+        {
+            var player = new Player();
+            Assert.Equal('X', player.Symbol);
+        }
+    }
+}
